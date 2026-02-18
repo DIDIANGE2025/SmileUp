e79e6dd969cfa9aa7f8772a4676aa11cd9b6eba8
DIDIANGE2025 <chatgptdidi2023@gmail.com>
2026-02-18 21:24:34 +0100

    feat(activity): ajout page détail activité avec participants et défi

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
+import { ActivityDetail } from './pages/activity-detail/activity-detail';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'dashboard', component: Dashboard },
   { path: 'admin-dashboard', component: AdminDashboard },
+  { path: 'activity/:id', component: ActivityDetail },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
