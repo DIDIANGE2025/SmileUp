af13568d1bcc70abf1fec6bee292a8d225762a96
DIDIANGE2025 <chatgptdidi2023@gmail.com>
2026-02-18 16:19:52 +0100

    Ajout Dashboard Admin et boutons de connexion différenciés

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'dashboard', component: Dashboard },
+  { path: 'admin-dashboard', component: AdminDashboard },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
